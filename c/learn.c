b44f087cd12f01cb1eb89b2b485c6b6850454f01
denikusumawijaya <112800492+denikusumawijaya@users.noreply.github.com>
2022-12-06 03:42:24 +0700

    Ongoing C Data Types

---
+++ c/learn.c
@@ -0,0 +1,27 @@
+//For input output
+#include <stdio.h>
+
+int main() {
+	printf("Hello World\n");
+	printf("I am learning C.");
+	printf("Insert a new line \n");
+	printf("Insert a backslash \\");
+	printf("Insert a double quote\"");
+	
+	//Single line comments
+	/*Multi line comments*/
+	
+	//Declaring variables
+	int myNumber = 5;
+	printf("%d\n", myNum);
+	float myFloatNum = 5.99;
+	printf("%f\n", myFloatNum);
+	char myLetter = 'D';
+	printf("%c\n", myLetter);
+	
+	printf("My favorite number is: %d", myNum);
+	
+	return 0;
+	
+	//On going C Data Types
+}
