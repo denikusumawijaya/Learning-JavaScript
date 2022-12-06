049d65bebe51981a96029119c61da068bd408873
denikusumawijaya <112800492+denikusumawijaya@users.noreply.github.com>
2022-12-06 16:21:53 +0700

    Ongoing String Search

--- javascript/learn.js
+++ javascript/learn.js
@@ -59,4 +59,66 @@ car["model"];
     }
   };
 
-//On progress JS Events
+//Events -> More HTML DOM
+//onchange, An HTML element has been changed
+//onclick, the user clicks an HTML element
+//onmouseover, the user moves the mouse over an HTML element
+//onmouseout, the user moves the mouse away from an HTML elemet
+//onkeydown, the user pushes a keyboard key
+//onload, the browser has finished the page
+
+<button onclick="document.getElementById('demo').innerHTML=Date()">The time is?</button>
+<p id="demo"></p>
+
+<button onclick="this.innerHTML=Date()">The time is?</button>
+<button onclick="displayDate()">The time is?</button>
+
+//Escape character
+// \b -> Backspace
+// \f -> From feed
+// \n -> New line
+// \r -> Carriage return
+// \t -> Horizontal tabulator
+// \v -> Vertical tabulator
+
+//String Methods
+let text = "Deni";
+text.length; //Return length of a string, Output: 5
+
+text = "Apple, Banana, Kiwi";
+text.slice(7, 13); //Return extracted part in a new string, Output: Banana
+text.slice(-12); //Output: Banana, Kiwi
+text.slice(-12, 6); //Output: Banana
+text.substring(7,13); //Output: Banana
+text.substr(7,6); //Output: Banana
+text.substr(7); //Output: Banana, Kiwi
+text.substr(-4); //Output: Kiwi
+
+text = "Please visit Micrososft!";
+text.replace("Microsoft", "W3Schools"); //Replace only first match, case sensitive
+text.replace(/MICROSOFT/i, "W3Schools"); //Incase sensitive, without quote
+text.replace(/Microsoft/g, "W3Schools"); //Global match
+text.replaceAll("Microsoft", "W3Schools");
+
+text.toUpperCase(); //Output: PLEASE VISIT MICROSOFT!
+text.toLowerCase(); //Oitput: please visit microsoft!
+text3 = text1.concat(" ", text2); //Same as: text1 + " " + text2
+text.trim(); //Before "   Test   " -> After "Test"
+text.trimStart(); //Before "   Test   " -> After "Test   "
+text.trimEnd(); //Before "   Test   " -> After "   Test"
+
+text = "5";
+text.padStart(4,"x"); //Output: xxx5, string method
+text = 5;
+text.toSting();
+text.padStart(4, "0"); //Output: 0005
+text.padEnd(4,xxx); //Output: 5xxx
+
+text = "HELLO WORLD";
+text.charAt(0); //Output: H
+text.carCodeAt(0); //Output: 72, UTF-16 code
+text[0];
+
+text.split(","); //Converted to Array
+
+//Ongoing String Search
