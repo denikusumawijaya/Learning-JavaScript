afb21a4243cd278de1f0fdeb65243b196f32d1f8
denikusumawijaya <112800492+denikusumawijaya@users.noreply.github.com>
2022-12-06 11:06:19 +0700

    Done

--- c/learn.c
+++ c/learn.c
@@ -9,6 +9,8 @@ int main() {
 	printf("Insert a new line \n");
 	printf("Insert a backslash \\");
 	printf("Insert a double quote\"");
+	printf("Insert a single quote \'");
+	printf("Insert a tab \t");
 	
 	//Single line comments
 	/*Multi line comments*/
@@ -101,6 +103,64 @@ int main() {
 		}
 		printf("%d\n", j);
 	}
+	for (j = 0; j < 10; j++) {
+		if (j == 4) {
+			continue;
+		}
+		printf("%d\n", j);
+	}
+	
+	//Arrays (can use for 2D array)
+	int myNumbers[] = {25, 50, 75, 100};
+	printf("%d", myNumbers[0]); //Access the elements
+	myNumbers[0] = 33; //Change an array elements
+	int myNUmbers_02[4]; //Set array size
+	myNymbers[0] = 25;
+	myNumbers[1] = 50;
+	myNumbers[2] = 75;
+	myNumbers[3] = 100;
+	
+	//C does not have a String type
+	//another way of creating strings
+	char greetings_02[] = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!', '\0'}; //Null terminating character, it tells C taht this is the end of the string.
+	printf("%lu\n", sizeof(greetings_02));
+	
+	//User input
+	int myInput;
+	printf("Type a number \n");
+	scanf("%d", &myInput); //Have to specify the reference operator (&)
+	printf("Your number is: %d", myInput);
+	
+	char firstName[30]; //Must specify the size of string
+	printf("Enter your first name: \n");
+	scanf("%s", firstName); //Do not have to specify the reference operator (&)
+	printf("Hello %s.", firstName);
+	
+	//Memory address
+	int myMemory = 43;
+	printf("%p", &myMemory); //Output: 0x7ffe5367e044 Probably won't get the smae result.
+	// &myMemory called pointer
+	//Creating Pointers
+	int myAge = 43; // an int variable
+	printf("%d", myAge);  // Outputs the value of myAge (43)
+	printf("%p", &myAge); // Outputs the memory address of myAge (0x7ffe5367e044)
+	
+	int myAge = 43;     // An int variable
+	int* ptr = &myAge;  // A pointer variable, with the name ptr, that stores the address of myAge
+	// Output the value of myAge (43)
+	printf("%d\n", myAge);
+	// Output the memory address of myAge (0x7ffe5367e044)
+	printf("%p\n", &myAge);
+	// Output the memory address of myAge with the pointer (0x7ffe5367e044)
+	printf("%p\n", ptr);
+	
+	//Dereference
+	int myAge = 43;     // Variable declaration
+	int* ptr = &myAge;  // Pointer declaration
+	// Reference: Output the memory address of myAge with the pointer (0x7ffe5367e044)
+	printf("%p\n", ptr);
+	// Dereference: Output the value of myAge with the pointer (43)
+	printf("%d\n", *ptr);
 	
 	return 0;
 }
